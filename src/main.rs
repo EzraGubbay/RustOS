9068ff57ba8743fb061fadb1778db423bb7f608f
Ezra Gubbay <ezragubbay99@gmail.com>
2026-07-14 10:38:18 +0300

    feat: Implemented test harness

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![no_std] // Disable use of Standard Library
 #![no_main] // Disable search for main function
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 mod vga_buffer;
 
@@ -11,12 +22,24 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     
     println!("Hello World{}", "!");
-    
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 // Custom panic handler, as we cannot use the one in Standard Library
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+// Test function
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    print!("[ok]");
 }
